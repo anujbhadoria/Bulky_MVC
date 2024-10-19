148886d2a4f0cfd6a2ea05fdc43f90dc1da5532b
Anuj Bhadoria <anujbhadoriacr7dmc@gmail.com>
2024-10-19 08:33:20 +0530

    Added Category class

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key] // Data annotation for making this prop primary key
+        public int Id { get; set; }
+        [Required] // To making this column required
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
+ 
